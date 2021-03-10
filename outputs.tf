ac0fc3d863180a643cb3ec80cc4ac5f7ec868da5
frol <frol.mr@ya.ru>
2021-03-10 19:23:58 +0300

    Initial commit

---
+++ outputs.tf
@@ -0,0 +1,27 @@
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "user_id" {
+  value = data.aws_caller_identity.current.user_id
+}
+
+output "region" {
+  value = data.aws_region.current.name
+}
+
+output "instance_ip_addr_0" {
+  value = aws_instance.web[0].private_ip
+}
+
+output "instance_ip_addr_1" {
+  value = aws_instance.web[1].private_ip
+}
+
+output "subnet_id_0" {
+  value = aws_instance.web[0].subnet_id
+}
+
+output "subnet_id_1" {
+  value = aws_instance.web[1].subnet_id
+}
